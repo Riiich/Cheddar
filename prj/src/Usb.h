b3ebaabcccbe9f3036501483dcd77d1d14bc3cea
Rich <dloorvae@gmail.com>
2014-02-05 14:50:06 +0800

    Add Handle_Suspend(). Modified based on Prolific_sample_0805.

--- prj/src/Usb.h
+++ prj/src/Usb.h
@@ -1,5 +1,6 @@
 ///// From UsbApi.c
 void UpdateUSB();
+void EventHdl_BusSuspend(void) ;
 unsigned char ctrlFIFORead(unsigned int len, unsigned char *buf);
 unsigned char ctrlFIFOWrite(unsigned int len, unsigned char *buf);
 
